a772a55c6410f39d8819ee7fd3228b524f125380
Rus V. Brushkoff <Rus@iSfinx.com>
2012-10-02 13:26:51 +0300

    Add TODO

--- version.cpp
+++ version.cpp
@@ -3,6 +3,6 @@
 
 // app version
 
-const char *OrientPP::ORIENTPP_CHANGESET = "6e787ec";
+const char *OrientPP::ORIENTPP_CHANGESET = "8b54c9d";
 int OrientPP::ORIENTPP_CHANGESET_NUMBER = 2;
 int OrientPP::ORIENTPP_BUILD_NUMBER = 0;
